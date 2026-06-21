52840183eeb1765e0bcdb2cd3fe53dadb539246a
Truite Séchée <truite.berthail@gmail.com>
2026-06-21 04:24:29 +0200

    Working prototype: now export the cards in a .apkg file you can import from Anki

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 # from dotenv import load_dotenv
 # import os
 from utils import *
+import genanki
+
+# define an anki model and deck
+champCardModel = genanki.Model(
+    2076546967,
+    "League of Legends champion card model",
+    fields=[
+        {"name": "Question"},
+        {"name": "Answer"}
+    ],
+    templates=[
+        {
+            "name": "Champion card",
+            "qfmt": "{{Question}}",
+            "afmt": "{{Answer}}",
+        }
+    ]
+)
+
+champsDeck = genanki.Deck(
+    2076546968,
+    "Champions League of Legends"
+)
+
 
 # load_dotenv()
 # RIOT_API_KEY = os.getenv("RIOT_API_KEY")
 # print("api key", RIOT_API_KEY)
+
 
 # first retrieve the current game version so we get the data from the good patch note
 PATCH_ID = get_patch_id()
@@ -14,8 +39,14 @@ print("language:", lang)
 
 champs = get_champs_list(PATCH_ID, lang)
 
+# champsDeck.add_model(champCardModel)
 for i in range(len(champs)):
     print(i, champs[i]["id"])
+    detailedChamp = get_champs_details(PATCH_ID, lang, champs[i]["id"])
+    champCard = genanki.Note(
+        model=champCardModel,
+        fields=[build_champ_card(PATCH_ID, detailedChamp, False), build_champ_card(PATCH_ID, detailedChamp, True)]
+    )
+    champsDeck.add_note(champCard)
 
-detailedChamp = get_champs_details(PATCH_ID, lang, champs[25]["id"])
-print(build_champ_card(PATCH_ID, detailedChamp, True))
+genanki.Package(champsDeck).write_to_file('LoLChampionsDeck.apkg')
